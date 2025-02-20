842cdd3409cd8bdbe8647fae37d08083ae304017
Alessandra Chioquetta <152203386+A-Chioquetta@users.noreply.github.com>
2025-02-20 15:26:33 -0300

    Update operators.py

--- operators.py
+++ operators.py
@@ -8,6 +8,7 @@ def generate_operators(N):
     sp = sigmap()
     sm = sigmam()
     p = (si - sz) / 2.0
+    q = (si + sz) / 2.0
 
     sx_list = []
     sy_list = []
@@ -15,6 +16,7 @@ def generate_operators(N):
     sp_list = []
     sm_list = []
     p_list = []
+    q_list = []
 
     for n in range(N):
         op_list = [si] * N
@@ -28,7 +30,16 @@ def generate_operators(N):
         op_list[n] = sz
         sz_list.append(tensor(op_list))
 
+        op_list[n] = sp
+        sp_list.append(tensor(op_list))
+
+        op_list[n] = sm
+        sm_list.append(tensor(op_list))
+
         op_list[n] = p
         p_list.append(tensor(op_list))
 
-    return sx_list, sy_list, sz_list, p_list
+        op_list[n] = q
+        q_list.append(tensor(op_list))
+
+    return sx_list, sy_list, sz_list,p_list, q_list, sp_list, sm_list 
